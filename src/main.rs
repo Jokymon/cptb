5446f08bb729de4b33e1ee222bb401c815059184
Silvan Wegmann <swegmann@narf.ch>
2020-05-02 20:20:39 +0200

    Add generation of main.cpp

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,18 @@ use std::io::prelude::*;
 #[folder = "templates"]
 struct Asset;
 
+fn copy_template_file(reg: &handlebars::Handlebars, rel_src_path: &str, project_name: &str) {
+    let template = Asset::get(rel_src_path).unwrap();
+    let mut template_file = File::create(format!("{}/{}", project_name, rel_src_path)).unwrap();
+    let file_content = reg
+        .render_template(
+            std::str::from_utf8(template.as_ref()).unwrap(),
+            &json!({ "projectname": project_name })
+        )
+        .unwrap();
+    template_file.write_all(file_content.as_ref()).unwrap();
+}
+
 fn main() {
     let matches = App::new("cptb")
         .version("1.0")
@@ -37,14 +49,7 @@ fn main() {
         fs::create_dir(object_name).expect("Couldn't create the directory");
         let reg = Handlebars::new();
 
-        let cmake_template = Asset::get("CMakeLists.txt").unwrap();
-        let mut cmake_file = File::create(format!("{}/CMakeLists.txt", object_name)).unwrap();
-        let cmake_content = reg
-            .render_template(
-                std::str::from_utf8(cmake_template.as_ref()).unwrap(),
-                &json!({ "projectname": object_name }),
-            )
-            .unwrap();
-        cmake_file.write_all(cmake_content.as_ref()).unwrap();
+        copy_template_file(&reg, "CMakeLists.txt", object_name);
+        copy_template_file(&reg, "main.cpp", object_name);
     }
 }
