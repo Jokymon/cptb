832a5ed5cbae845a80ede078dbbbf0866b8a161a
Silvan Wegmann <silvan.wegmann@bbv.ch>
2021-01-18 15:33:17 +0100

    Use detected toolchain/target/CMake info for generated projects

--- src/project_builder.rs
+++ src/project_builder.rs
@@ -4,8 +4,12 @@ use rust_embed::RustEmbed;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
+use tempdir::TempDir;
 
+use crate::cmake::CmakeBuilder;
 use crate::error::CptbError;
+use crate::settings::Settings;
 
 #[derive(RustEmbed)]
 #[folder = "templates"]
@@ -14,7 +18,23 @@ struct Asset;
 #[derive(Serialize)]
 struct TemplateParameters {
     projectname: String,
+    toolchain: String,
+    target: String,
+    cmake_version: String,
     static_build: bool,
+}
+
+#[derive(Deserialize)]
+struct SystemDescription {
+    toolchain: String,
+    target: String,
+    cmake_version: String,
+}
+
+fn copy_asset_to_target(source_template_path: &str, target_file_path: &str) {
+    let template = Asset::get(source_template_path).unwrap();
+    let mut target_file = File::create(target_file_path).unwrap();
+    target_file.write_all(template.as_ref()).unwrap();
 }
 
 fn copy_template_file_to_target(
@@ -49,10 +69,42 @@ fn download_file(source_url: &str, target_file_path: &str) -> Result<(), CptbErr
     Ok(())
 }
 
+fn get_system_description() -> SystemDescription {
+    let tmp_dir = TempDir::new("system_description").unwrap();
+
+    copy_asset_to_target(
+        "detection/CMakeLists.txt",
+        tmp_dir.path().join("CMakeLists.txt").to_str().unwrap(),
+    );
+    copy_asset_to_target(
+        "detection/main.cpp",
+        tmp_dir.path().join("main.cpp").to_str().unwrap(),
+    );
+
+    let cmake_builder = CmakeBuilder::from_settings(&Settings::from_home().unwrap());
+    cmake_builder.generate(
+        tmp_dir.path().to_str().unwrap(),
+        tmp_dir.path().join("build").to_str().unwrap(),
+    );
+
+    let build_dir = tmp_dir.path().join("build");
+    let status_file_path = build_dir.join("status.json");
+    let file = File::open(status_file_path.to_str().unwrap()).unwrap();
+    let reader = BufReader::new(file);
+    let system_description: SystemDescription = serde_json::from_reader(reader).unwrap();
+
+    system_description
+}
+
 pub fn cptb_new_command(project_name: &str, static_build: bool) {
+    let system_description = get_system_description();
+
     let template_parameters = TemplateParameters {
         projectname: project_name.to_string(),
         static_build: static_build,
+        toolchain: system_description.toolchain,
+        target: system_description.target,
+        cmake_version: system_description.cmake_version,
     };
 
     fs::create_dir(project_name).expect("Couldn't create the directory");
